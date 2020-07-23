691ed55c3d727338719c40de19abbf361489f9e3
Gaby Mendez <gmendez90@gmail.com>
2020-07-23 14:50:05 -0400

    Write passwords#update action

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,9 +1,23 @@
 class PasswordsController < ApplicationController
   before_action :current_user
-  
+
   def edit
   end
 
   def update
+    current_user.update(pass_params)
+    if current_user.valid?
+      redirect_to "/profile"
+      flash[:success] = 'Your password has been updated.'
+    else
+      flash[:errors] = current_user.errors.full_messages
+      redirect_to "/passwords/edit"
+    end
+  end
+
+  private
+
+  def pass_params
+    params.permit(:password)
   end
 end
