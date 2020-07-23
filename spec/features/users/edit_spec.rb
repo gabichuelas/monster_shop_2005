6d6f8426f0cd9e949aa709f317d66595a3bb4d65
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-23 14:02:01 -0600

    Add spec for us22

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe 'As a registered user' do
                                   password: "qwer",
                                   role: 0)
 
+    @existing_merchant = User.create!(name: "Jake from StateFarm",
+                                  address: "Mechant St.",
+                                  city: "Denver",
+                                  state: "Colorado",
+                                  zip: "80202",
+                                  email: "example_merchant@hotmail.com",
+                                  password: "qwer",
+                                  role: 1)
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
 
@@ -31,6 +40,28 @@ RSpec.describe 'As a registered user' do
 
       expect(current_path).to eq("/profile")
       expect(page).to have_content('Boulder, CO')
+    end
+
+    describe "When I attempt to edit my profile data and input an email address that is already in the system" do
+      it "when I submit the form, a flash message tells me that email is already in use and I am returned to the profile edit page" do
+
+        visit "/profile"
+        click_on 'Edit Profile'
+        expect(current_path).to eq("/users/edit")
+
+        expect(find_field('Name').value).to eq('Bob Vance')
+        expect(find_field('Address').value).to eq('123 ABC St.')
+        expect(find_field('City').value).to eq('Denver')
+        expect(find_field('State').value).to eq('CO')
+        expect(find_field('Zip').value).to eq('80202')
+        expect(find_field('Email').value).to eq('example@hotmail.com')
+
+        fill_in :email, with: @existing_merchant.email
+        click_on 'Save Profile'
+
+        expect(current_path).to eq("/users/edit")
+        expect(page).to have_content("Email has already been taken")
+      end
     end
   end
 end
