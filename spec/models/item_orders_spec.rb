cde003a8e3b56be2bd21633552e78a17141fa7ab
Norma Lopez <norma_l304@yahoo.com>
2020-07-24 21:08:25 -0600

    Create user and add user to order

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -15,9 +15,18 @@ describe ItemOrder, type: :model do
 
   describe 'instance methods' do
     it 'subtotal' do
+      user = User.create!(name: "Bob Vance",
+                                    address: "123 ABC St.",
+                                    city: "Denver",
+                                    state: "CO",
+                                    zip: "80202",
+                                    email: "example@hotmail.com",
+                                    password: "qwer",
+                                    role: 0)
+
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_1 = user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
       item_order_1 = order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2)
 
       expect(item_order_1.subtotal).to eq(200)
