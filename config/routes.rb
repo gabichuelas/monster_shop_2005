4f441a0e86bfceb7aa253172b6dcae4b72d1bde1
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-26 12:12:50 -0600

    Add route to ship order in namespace admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get "/", to: "dashboard#index"
     get "/users/:user_id", to: "users#show"
+    patch "/orders/:id", to: "orders#ship"
   end
 
   namespace :merchant do
