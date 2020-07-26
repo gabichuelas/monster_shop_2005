3251819cf9916bfe02b7d54b3e32f2308d6a6f7a
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-26 13:40:05 -0600

    Add show method and comment it out until route is corrected

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -3,4 +3,9 @@ class Merchant::DashboardController < ApplicationController
 
   def index
   end
+
+  # def show  <----- made this when I did the routes wrong. Hoping this will work when the routes are corrected. 
+  #   if current_user.merchant_id != nil
+  #   @merchant = Merchant.find(current_user.merchant_id)
+  # end
 end
