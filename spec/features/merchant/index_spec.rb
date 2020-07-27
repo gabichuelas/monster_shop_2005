a8ee5b853f78c4ecaed660c17b47aeb4efe52561
Gaby Mendez <gmendez90@gmail.com>
2020-07-26 22:41:39 -0400

    Write spec for us36 on merchant dashboard

---
+++ spec/features/merchant/index_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe 'As a merchant employee' do
+  before :each do
+    @merchant_employee = User.create!(name: "Jose", address: "789 Jkl St.", city: "Denver", state: "Colorado", zip: "80202", email: "example4@hotmail.com", password: "qwer", role: 1)
+  end
+
+  describe 'When I visit my merchant dashboard' do
+    it 'I see a link to view my own items; When I click that link; My URI route should be "/merchant/items"' do
+
+      visit "/merchant"
+      click_on "View My Items"
+      expect(current_path).to eq("/merchant/items")
+    end
+  end
+end
