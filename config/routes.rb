3ed7e528d7e2ae4f683b7b05f0e63e7bdafac7a4
Gaby Mendez <gmendez90@gmail.com>
2020-07-28 15:13:01 -0400

    Rebase with updated master

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
     get "/", to: "dashboard#show"
     get "/items", to: "items#index"
     get "/orders/:id", to: "orders#show"
+
     patch '/items/:id/update', to: 'items#update'
+    get "/items/new", to: "items#new"
+    post "/items", to: "items#create"
+
   end
 
   get "/", to: "welcome#index"
