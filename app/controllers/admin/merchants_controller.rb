6912f062d1cabfd22c2455286a169d62213455e0
Gaby Mendez <gmendez90@gmail.com>
2020-07-27 20:43:52 -0400

    Edit admin/merchants#update to include methods for enabling and disabling items

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,9 +7,11 @@ class Admin::MerchantsController < ApplicationController
     merchant = Merchant.find(params[:id])
     if merchant.enabled == true
       merchant.update(enabled: false)
+      merchant.disable_items
       flash[:notice] = "This merchant\'s account is now disabled."
     else
-      merchant.update(enabled: true) #<---- this may be wrong?
+      merchant.update(enabled: true)
+      merchant.enable_items
       flash[:notice] = "This merchant\'s account is now enabled."
     end
     redirect_to "/merchants"
