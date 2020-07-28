11373be65285108038eb9392c248f53e17bc0cd9
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-27 18:06:45 -0600

    Add update method to admin/merchants controller for enabling or disabling a merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,4 +2,16 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if merchant.enabled == true
+      merchant.update(enabled: !merchant.enabled)
+      flash[:notice] = "This merchant\'s account is now disabled."
+    else
+      merchant.update(enabled: merchant.enabled) #<---- this may be wrong?
+      flash[:notice] = "This merchant\'s account is now enabled."
+    end
+    redirect_to "/merchants"
+  end
 end
