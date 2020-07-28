ec402a892820d1f5a4f03a6a6bc4e592870d976c
Ashkan Abbasi <airashkan@yahoo.com>
2020-07-28 12:00:56 -0600

    Create US47 spec

--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -52,6 +52,36 @@ RSpec.describe 'As a merchant employee' do
 
     end
   end
+
+  describe "When I visit my items page" do
+    it "can edit an items info" do
+      visit "/merchant/items"
+
+      within "#item-#{@tire.id}" do
+        expect(page).to_not have_button('Edit')
+        click_on "Edit"
+      end
+
+      expect(current_path).to eq("/merchant/items/#{@tire.id}/edit")
+
+      expect(page).to have_content(@tire.name)
+      expect(page).to have_content(@tire.description)
+      expect(page).to have_content(@tire.price)
+      expect(page).to have_content(@tire.image)
+      expect(page).to have_content(@tire.inventory)
+
+      fill_in "Description", with: "Indestructable"
+      click_on "Submit"
+
+      expect(current_path).to eq("/merchant/items")
+      expect(page).to have_content("#{@tire.name} has been updated")
+
+      within "#item-#{@tire.id}" do
+        expect(page).to have_content("Indestructable")
+      end
+
+    end
+  end
 end
 
 
@@ -64,3 +94,21 @@ end
 # I am returned to my items page
 # I see a flash message indicating this item is now deleted
 # I no longer see this item on the page
+
+# User Story 47, Merchant edits an item
+#
+# As a merchant employee
+# When I visit my items page
+# And I click the edit button or link next to any item
+# Then I am taken to a form similar to the 'new item' form
+# The form is pre-populated with all of this item's information
+# I can change any information, but all of the rules for adding a new item still apply:
+# - name and description cannot be blank
+# - price cannot be less than $0.00
+# - inventory must be 0 or greater
+#
+# When I submit the form
+# I am taken back to my items page
+# I see a flash message indicating my item is updated
+# I see the item's new information on the page, and it maintains its previous enabled/disabled state
+# If I left the image field blank, I see a placeholder image for the thumbnail
