eb03922b9bb49cbc29cc81b0e3b1e89903680f5f
Gaby Mendez <gmendez90@gmail.com>
2020-07-29 16:02:37 -0400

    Change /merchants to /admin/merchants for admin/merchants/index spec

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,6 @@
 class Admin::OrdersController < ApplicationController
+  before_action :require_admin
+
   def update
     @order = Order.find(params[:id])
     @order.update(status: params[:status].to_i)
