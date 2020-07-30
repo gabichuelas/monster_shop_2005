c56ace153d0693cea2ec873a2412f806d8d8b66b
Norma Lopez <norma_l304@yahoo.com>
2020-07-29 20:31:12 -0600

    Add if statement to check if all items are fulfilled and change toggle! to update

--- app/controllers/merchant/item_orders_controller.rb
+++ app/controllers/merchant/item_orders_controller.rb
@@ -1,11 +1,15 @@
 class Merchant::ItemOrdersController < ApplicationController
   before_action :require_merchant
-  
+
   def update
     item_order = ItemOrder.find(params[:id])
-    item_order.toggle!(:status)
+    order = Order.find(item_order.order_id)
+    item_order.update(status: "fulfilled")
     item_order.item.update!(inventory: item_order.item.inventory - item_order.quantity)
     flash[:success] = "This order has been fulfilled"
     redirect_to "/merchant/orders/#{item_order.order.id}"
+    if order.item_orders.all? { |item_order| item_order.status == "fulfilled" }
+      order.update(status: "packaged")
+    end
   end
 end
