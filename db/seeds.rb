fc35d5ca834823326c85c3e43420b6ebd9e7ee4f
Norma Lopez <norma_l304@yahoo.com>
2020-07-30 13:45:39 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,11 @@
 Merchant.destroy_all
 Item.destroy_all
 
+#merchants
+bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+surf_shop = Merchant.create(name: "Corey's Surf Shop", address: '77 Beach Way', city: 'San Diego', state: 'CA', zip: 90210)
+
 #users
 @admin = User.create!(name: "Bob Vance",
                    address: "123 ABC St.",
@@ -39,10 +44,6 @@ Item.destroy_all
                      role: 0)
 
 
-#merchants
-bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
-surf_shop = Merchant.create(name: "Corey's Surf Shop", address: '77 Beach Way', city: 'San Diego', state: 'CA', zip: 90210)
 
 #bike_shop items
 tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
