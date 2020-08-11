ab21716c0f8ba928f26798867fba0234b475c3bd
Gaby Mendez <gmendez90@gmail.com>
2020-08-11 12:14:36 -0400

    Refactor items resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get "/", to: "welcome#index"
+
   namespace :admin do
     get "/", to: "dashboard#index"
 
@@ -32,27 +34,30 @@ Rails.application.routes.draw do
     # patch  "/item_orders/:id", to: "item_orders#update"
   end
 
-  get "/", to: "welcome#index"
+  resources :merchants do
+  # get "/merchants", to: "merchants#index"
+  # get "/merchants/new", to: "merchants#new"
+  # get "/merchants/:id", to: "merchants#show"
+  # post "/merchants", to: "merchants#create"
+  # get "/merchants/:id/edit", to: "merchants#edit"
+  # patch "/merchants/:id", to: "merchants#update"
+  # delete "/merchants/:id", to: "merchants#destroy"
+    resources :items, only: [:index, :new, :create]
+    # get "/merchants/:merchant_id/items", to: "items#index"
+    # get "/merchants/:merchant_id/items/new", to: "items#new"
+    # post "/merchants/:merchant_id/items", to: "items#create"
+  end
 
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
-
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
-  get "/merchants/:merchant_id/items", to: "items#index"
-  get "/merchants/:merchant_id/items/new", to: "items#new"
-  post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
+  # resources :items, except: [:new, :create]
+  # get "/items", to: "items#index"
+  # get "/items/:id", to: "items#show"
+  # get "/items/:id/edit", to: "items#edit"
+  # patch "/items/:id", to: "items#update"
+  #
+  # delete "/items/:id", to: "items#destroy"
 
   resources :reviews, only: [:edit, :update, :destroy]
-  resources :items do
+  resources :items, except: [:new, :create] do
     resources :reviews, only: [:new, :create]
   end
 
